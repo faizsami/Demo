0397764ec901b1d6f2a9282388e14997da4acb05
Azeem <fdl.techinspire@gmail.com>
2023-07-15 14:25:48 +0530

    new

--- app/src/main/java/com/techinspire/lockdown/MainActivity.java
+++ app/src/main/java/com/techinspire/lockdown/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent();
+        startActivity(intent);
 
 
 
