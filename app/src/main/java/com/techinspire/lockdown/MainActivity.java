18bde953296ea872924de6143e4de82b3efd7034
faizs <faizsami96@gmail.com>
2023-07-15 14:14:53 +0530

    updated

--- app/src/main/java/com/techinspire/lockdown/MainActivity.java
+++ app/src/main/java/com/techinspire/lockdown/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = new Intent();
 
 
 
